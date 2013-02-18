edc0d744902329e67ba2dd99c5c824e16da7dca8
Yuki Matsukura <matsubokkuri@gmail.com>
2013-02-19 00:43:21 +0900

    added setting for heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module DeviseSample
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # devise setting for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
