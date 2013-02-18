25feb61e8257be7a783d9153df12475f98896ab6
Yuki Matsukura <matsubokkuri@gmail.com>
2013-02-19 00:36:19 +0900

    rails new devise_smaple

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseSample::Application.config.secret_token = 'a3943c89f9405035217e9f5f57f5c1ce6fc8fb29f241676143cb64f6608c04af493e6582f1b13147dc5d1781b22d4a182de5d2a3e764bbe94062e042c4f5e32b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseSample::Application.config.session_store :cookie_store, key: '_devise_sample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviseSample::Application.config.session_store :active_record_store
